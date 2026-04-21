f91f4fa4ebfa7d2bd549c169c4a6a9c613918377
Devoy Douglas <devoydouglas@gmail.com>
2026-04-21 15:30:17 -0500

    Set baseURL for application in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-01-01',
 
   app: {
+    baseURL: '/Techtron Live Limited/',
     head: {
       title: 'Techtron Live Limited',
       meta: [
